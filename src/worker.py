8fe5daab025568d96e79d83f35f76067ecfb38e0
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2026-04-02 16:07:05 +0530

    auth routes init

--- src/worker.py
+++ src/worker.py
@@ -33,12 +33,39 @@ def ok(data=None, msg: str = "OK"):
 def err(msg: str, status: int = 400):
     return json_resp({"error": msg}, status)
 
+async def parse_json_object(req):
+    try:
+        text = await req.text()
+        body = json.loads(text)
+    except Exception:
+        return None, err("Invalid JSON body")
+
+    if not isinstance(body, dict):
+        return None, err("JSON body must be an object", 400)
+
+    return body, None
+
+########
 
 async def api_home(request, env):
     return ok({
         "message": "Welcome Home, Hello world!"
     })
 
+async def api_signup(req, env):
+    body, badResponse = await parse_json_object(req)
+    if badResponse:
+        return badResponse
+    
+    username = body.get("username")
+    email = body.get("email")
+    password = body.get("password")
+    public_key = body.get("public_key")
+
+    if not username or not email or not password or not public_key:
+        return err("username, email, password, publicKey: Something is missing!")
+
+    return ok(None, "SIGNUPED! successfully")
 
 async def _dispatch(request, env):
     path = urlparse(request.url).path
@@ -49,6 +76,9 @@ async def _dispatch(request, env):
     
     if path == "/test" and method == "GET":
         return await api_home(request, env)
+    
+    if path == "/api/auth/signup" and method == "POST":
+        return await api_signup(request, env)
     
 async def on_fetch(request, env):
     try:
